69685dd574ada5dfc48d53337d47d37df4a23e57
vicvrg <victor.rodriguez@uva.es>
2025-10-15 15:19:30 +0200

    Changed assignement by assignment :)
    Former-commit-id: 33a5dee324bfab55268952dde16923f04d3dbf99 [formerly 32eaf21af01988a82d5e549209dd0882b5fc2e7d]
    Former-commit-id: 4da98eeb8bfb5b19d84ac9bc948a44ddbe826056

--- plots_and_stats/group_assignment/controller.py
+++ plots_and_stats/group_assignment/controller.py
@@ -1,7 +1,7 @@
 from PySide6 import QtWidgets, QtCore, QtGui
 from pathlib import Path
 
-class AssignementController(QtCore.QObject):
+class AssignmentController(QtCore.QObject):
     def __init__(self, ui):
         super().__init__()
         self.view = ui
@@ -14,25 +14,25 @@ class AssignementController(QtCore.QObject):
         else:
             self.all_items = self.view.main_module.controller.subjects
 
-        self.view.tableAssignement.setRowCount(len(self.all_items))
+        self.view.tableAssignment.setRowCount(len(self.all_items))
         for idx_rec, recording in enumerate(self.all_items):
             # Recording name
             # name = Path(recording).stem
-            self.view.tableAssignement.setItem(idx_rec, 0, QtWidgets.QTableWidgetItem(str(recording)))
+            self.view.tableAssignment.setItem(idx_rec, 0, QtWidgets.QTableWidgetItem(str(recording)))
 
         # Button connect
         self.view.main_module.nextButton.setEnabled(False)
 
         # Allow right-click context menu
-        self.view.tableAssignement.setContextMenuPolicy(QtCore.Qt.ContextMenuPolicy.CustomContextMenu)
+        self.view.tableAssignment.setContextMenuPolicy(QtCore.Qt.ContextMenuPolicy.CustomContextMenu)
         # Right-click event calls the show_context_menu method
-        self.view.tableAssignement.customContextMenuRequested.connect(self.show_context_menu)
+        self.view.tableAssignment.customContextMenuRequested.connect(self.show_context_menu)
 
         # First usage
         self.view.shown.connect(self.on_show_event)
 
         # # Connect
-        # self.view.tableAssignement.cellChanged.connect(self.on_cell_changed)
+        # self.view.tableAssignment.cellChanged.connect(self.on_cell_changed)
 
 
     def show_context_menu(self, pos):
@@ -47,7 +47,7 @@ class AssignementController(QtCore.QObject):
             action.triggered.connect(lambda checked, g=group: self.assign_group_to_selected(g))
 
         # Run the context menu at the cursor position
-        menu.exec_(self.view.tableAssignement.viewport().mapToGlobal(pos))
+        menu.exec_(self.view.tableAssignment.viewport().mapToGlobal(pos))
 
     def assign_group_to_selected(self, group_str):
         """
@@ -55,15 +55,15 @@ class AssignementController(QtCore.QObject):
         """
 
         # Get the selected rows
-        selected_rows = set(idx.row() for idx in self.view.tableAssignement.selectionModel().selectedIndexes())
+        selected_rows = set(idx.row() for idx in self.view.tableAssignment.selectionModel().selectedIndexes())
         # Assign the group to each selected row
         for row in selected_rows:
-            self.view.tableAssignement.setItem(row, 1, QtWidgets.QTableWidgetItem(group_str))
+            self.view.tableAssignment.setItem(row, 1, QtWidgets.QTableWidgetItem(group_str))
 
             # Change the background color of the row to the group's color
             color = self.view.main_module.controller.groups[group_str]
-            for col in range(self.view.tableAssignement.columnCount()):
-                item = self.view.tableAssignement.item(row, col)
+            for col in range(self.view.tableAssignment.columnCount()):
+                item = self.view.tableAssignment.item(row, col)
                 qt_color = QtGui.QColor(color)
                 qt_color.setAlpha(100) # Set transparency
                 item.setBackground(qt_color)
@@ -100,12 +100,13 @@ class AssignementController(QtCore.QObject):
             self.first_show = False
 
     def on_cell_changed(self):
+
+        ## Count the number of subjects in each group
         # Create an empty count dictionary
         group_counts = {k: 0 for k in self.view.main_module.controller.groups}
-
         # Groups are in column 1
-        for r in range(self.view.tableAssignement.rowCount()):
-            item = self.view.tableAssignement.item(r, 1)
+        for r in range(self.view.tableAssignment.rowCount()):
+            item = self.view.tableAssignment.item(r, 1)
             if item:
                 group = item.text()
                 if group:
@@ -116,3 +117,16 @@ class AssignementController(QtCore.QObject):
             label = self.view.findChild(QtWidgets.QLabel, group.replace(' ', ''))
             if label:
                 label.setText(f"{group}: {group_counts[group]} subject(s)")
+
+        ## Store the group assignment in the controller of the main_module
+        # Create an empty count dictionary
+        group_assignment = {k: [] for k in self.view.main_module.controller.groups}
+        # Groups are in column 1
+        for r in range(self.view.tableAssignment.rowCount()):
+            item = self.view.tableAssignment.item(r, 1)
+            if item:
+                group = item.text()
+                subject = self.view.tableAssignment.item(r, 0).text()
+                if group:
+                    group_assignment[group].append(subject)
+        self.view.main_module.controller.group_assignment = group_assignment
--- plots_and_stats/group_assignment/view.py
+++ plots_and_stats/group_assignment/view.py
@@ -1,9 +1,9 @@
 from PySide6 import QtWidgets, QtCore, QtGui
 from PySide6.QtUiTools import loadUiType
 
-ui_plots_groups = loadUiType('plots_and_stats/group_assignement/view.ui')[0]
+ui_plots_groups = loadUiType('plots_and_stats/group_assignment/view.ui')[0]
 
-class AssignementWidget(QtWidgets.QWidget, ui_plots_groups):
+class AssignmentWidget(QtWidgets.QWidget, ui_plots_groups):
     shown = QtCore.Signal()
     def __init__(self, main_module):
         super().__init__()
@@ -11,27 +11,27 @@ class AssignementWidget(QtWidgets.QWidget, ui_plots_groups):
         self.main_module = main_module
 
         # Initial table setup, 0 rows, 2 columns with names "Group Name" and "Color"
-        self.tableAssignement.setRowCount(0)
-        self.tableAssignement.setColumnCount(2)
-        self.tableAssignement.setHorizontalHeaderLabels(["Subject", "Group"])
+        self.tableAssignment.setRowCount(0)
+        self.tableAssignment.setColumnCount(2)
+        self.tableAssignment.setHorizontalHeaderLabels(["Subject", "Group"])
         # Configures the column resize mode to be interactive and prevents the last column from stretching automatically.
-        self.tableAssignement.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeMode.Interactive)
-        self.tableAssignement.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeMode.Interactive)
-        self.tableAssignement.horizontalHeader().setStretchLastSection(False)
+        self.tableAssignment.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeMode.Interactive)
+        self.tableAssignment.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeMode.Interactive)
+        self.tableAssignment.horizontalHeader().setStretchLastSection(False)
 
         # Override the table's resize event to call resizeColumns whenever the size changes, and call the function once
         # at the start to adjust the widths.
-        self.tableAssignement.resizeEvent = lambda event: (self.resize_columns(),
-              QtWidgets.QTableWidget.resizeEvent(self.tableAssignement, event))
+        self.tableAssignment.resizeEvent = lambda event: (self.resize_columns(),
+              QtWidgets.QTableWidget.resizeEvent(self.tableAssignment, event))
         self.resize_columns()
 
         # Allow multiple row selection
-        self.tableAssignement.setSelectionBehavior(QtWidgets.QTableWidget.SelectionBehavior.SelectRows)
-        self.tableAssignement.setSelectionMode(QtWidgets.QTableWidget.SelectionMode.ExtendedSelection)
+        self.tableAssignment.setSelectionBehavior(QtWidgets.QTableWidget.SelectionBehavior.SelectRows)
+        self.tableAssignment.setSelectionMode(QtWidgets.QTableWidget.SelectionMode.ExtendedSelection)
         # Overwrite mouse press event to clean the previous selection if Ctrl or Shift are not pressed
-        self.tableAssignement.mousePressEvent = self.table_mouse_press
+        self.tableAssignment.mousePressEvent = self.table_mouse_press
         # Do not allow editing the table cells
-        self.tableAssignement.setEditTriggers(QtWidgets.QTableWidget.EditTrigger.NoEditTriggers)
+        self.tableAssignment.setEditTriggers(QtWidgets.QTableWidget.EditTrigger.NoEditTriggers)
 
         # Search line
         self.searchEdit.setPlaceholderText("Find recordings...")
@@ -59,26 +59,26 @@ class AssignementWidget(QtWidgets.QWidget, ui_plots_groups):
             # This makes a normal click start a new selection instead of adding to the old one
             if not (modifiers & QtCore.Qt.KeyboardModifier.ControlModifier or
                     modifiers & QtCore.Qt.KeyboardModifier.ShiftModifier):
-                self.tableAssignement.clearSelection()
+                self.tableAssignment.clearSelection()
 
             # Call the original mousePressEvent to handle the actual row selection
-            QtWidgets.QTableWidget.mousePressEvent(self.tableAssignement, event)
+            QtWidgets.QTableWidget.mousePressEvent(self.tableAssignment, event)
 
 
     def resize_columns(self):
         """
-        Adjust the column widths of the tableAssignement based on the current width of the viewport, the first column
+        Adjust the column widths of the tableAssignment based on the current width of the viewport, the first column
         takes 80% of the width and the second column takes 20%.
         """
-        total_width = self.tableAssignement.viewport().width()
-        self.tableAssignement.setColumnWidth(0, int(total_width * 0.8))
-        self.tableAssignement.setColumnWidth(1, int(total_width * 0.2))
+        total_width = self.tableAssignment.viewport().width()
+        self.tableAssignment.setColumnWidth(0, int(total_width * 0.8))
+        self.tableAssignment.setColumnWidth(1, int(total_width * 0.2))
 
     def filter_items(self, text):
         """Filter the table rows by text in the 'Subject' column."""
         text = text.lower().strip()
-        for row in range(self.tableAssignement.rowCount()):
-            item = self.tableAssignement.item(row, 0)  # columna 'Subject'
+        for row in range(self.tableAssignment.rowCount()):
+            item = self.tableAssignment.item(row, 0)  # columna 'Subject'
             if item:
                 # Show or hide the row based on whether the text is found
-                self.tableAssignement.setRowHidden(row, text not in item.text().lower())
+                self.tableAssignment.setRowHidden(row, text not in item.text().lower())
