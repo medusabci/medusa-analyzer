28498c57138bcf013e56d69914c738b7c4eea37b
vicvrg <victor.rodriguez@uva.es>
2025-11-11 10:03:36 +0100

    Added experiment template

---
+++ experiment_template/step_1/controller.py
@@ -0,0 +1,36 @@
+import numpy as np
+from PySide6 import QtWidgets
+import json
+
+class Step1Controller:
+    def __init__(self, ui):
+        self.view = ui
+        self.view.controller = self
+        self.first_show = False
+
+        # Connects
+        self.view.button1.toggled.connect(self.on_button1_toggle)
+        self.view.button2.toggled.connect(self.on_button2_toggle)
+
+        # Set initial state
+        self.view.shown.connect(self.on_show_event)
+
+
+    def on_button1_toggle(self, checked):
+        # Do something when button1 is toggled
+        print('Button 1 toggled')
+        for widget in [self.view.label1, self.view.text1]:
+            widget.setVisible(checked)
+
+
+    def on_button2_toggle(self, checked):
+        # Do something when button1 is toggled
+        print('Button 2 toggled')
+        for widget in [self.view.label2, self.view.text2]:
+            widget.setVisible(checked)
+
+    def on_show_event(self):
+        # Do something when the widget is shown
+        if not self.first_show:
+            self.first_show = True
+            print('Step 1 widget shown for the first time')
---
+++ experiment_template/step_2/view.py
@@ -0,0 +1,68 @@
+from PySide6 import QtWidgets, QtGui, QtCore
+from PySide6.QtUiTools import loadUiType
+
+# Load UI class
+ui_step_2 = loadUiType('experiment_template/step_2/view.ui')[0]
+
+class Step2Widget(QtWidgets.QWidget, ui_step_2):
+    shown = QtCore.Signal() # Signal to be emitted when the widget is shown
+
+    def __init__(self, main_window):
+        super().__init__()
+        self.setupUi(self)
+        self.main_window = main_window
+
+        ### DATA LOADER HEADER ###
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(10)
+        self.topContentWidget.setLayout(layout)
+
+        self.description_label = QtWidgets.QLabel()
+        self.description_label.setTextFormat(QtCore.Qt.RichText)
+        self.description_label.setWordWrap(True)
+        self.description_label.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.description_label.setText("""
+            <div style="text-align:center; font-family:'Segoe UI', Arial;">
+                <p style="font-size: 12pt; color:#444; margin:0 40px 10px 40px;">
+                    Header of the widget.
+                </p>
+
+                <p style="font-size: 11pt; color:#666; margin:0 40px;">
+                    Explanations <b style="color:#007acc;">explanations</b>, and <b>more</b> explanations.
+                </p>
+            </div>
+        """)
+
+        # Remove background
+        palette = QtGui.QPalette()
+        palette.setColor(QtGui.QPalette.Base, palette.color(QtGui.QPalette.Window))
+        self.topContentWidget.setPalette(palette)
+        self.topContentWidget.setFrameShape(QtWidgets.QFrame.NoFrame)
+
+        layout.addWidget(self.description_label)
+
+
+        ### ELEMENT CONFIGURATION ###
+
+        # Set initial state
+        for widget in [self.label1, self.label2, self.text1, self.text2]:
+            widget.setVisible(False)
+
+        # Set default values for something
+        self.box1.setValue(1)
+        self.box2.setValue(2)
+
+        # Default values in a dict
+        self.defaults = {
+            "box1": self.box1.value(),
+            "box2": self.box2.value(),
+            "some_text1": self.text1.text(),
+            "some_text2": self.text2.text()
+        }
+
+    def showEvent(self, event):
+        super().showEvent(event)
+        self.shown.emit() # Signal emission
