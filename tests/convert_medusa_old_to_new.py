517658bf0d8f7025e3e82d2906d23b85c46de218
vicvrg <victor.rodriguez@uva.es>
2026-09-22 11:15:29 +0200

    Included splash screen in EXE

--- tests/convert_medusa_old_to_new.py
+++ tests/convert_medusa_old_to_new.py
@@ -1,13 +1,15 @@
 from medusa.core.legacy.recording import Recording as LegacyRecording
-from medusa.core.legacy.convert import recorder_recording_to_v2
+from medusa.core.legacy.convert import edubiomat_recording_to_v2
+# from medusa.core import components
 from pathlib import Path
 
-files = list(Path(r"X:\Temps\braingym").rglob("*.bson"))
+files = list(Path(r"C:\Users\1993_\Desktop\OneDrive_1_21-9-2026\Claramente nervioso").rglob("*.bson"))
 
-for file in files:
+for idx,file in enumerate(files):
     print(files)
     mds_old = LegacyRecording.load(str(file))
-    subject = str(file.parent.name).split('-')[1]
-    mds_new = recorder_recording_to_v2(mds_old)
+    # subject = str(file.parent.name).split('-')[1]
+    subject = str(idx)
+    mds_new = edubiomat_recording_to_v2(mds_old)
     mds_new.bids.subject = subject
-    mds_new.save(rf'X:\Temps\braingym_new\sub-{mds_new.bids.subject}_task-{mds_new.bids.task}.h5', data_format='h5')
+    mds_new.save(rf'C:\Users\1993_\Desktop\AAAAA\sub-{mds_new.bids.subject}_task-{mds_new.bids.task}.h5', data_format='h5')
